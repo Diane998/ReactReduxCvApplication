a8bcdc7678675ebd48d90eac2accf8cb29166dff
Diane998 <gutierrez.almiradyan@gmail.com>
2020-12-03 06:34:27 +0800

    firebase authentication

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,103 @@
 import history from '../history';
 import { reset } from 'redux-form';
+import { getFirestore } from 'redux-firestore';
+
+export const signInWithGoogle = credentials => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+
+  firebase
+    .login({
+      provider: 'google',
+      type: 'popup'
+    })
+    .then(({ credential: { signInMethod }, profile: { uid } }) => {
+      dispatch({
+        type: 'SIGN_IN_WITH_GOOGLE',
+        payload: { signInMethod, uid }
+      });
+    });
+};
+
+export const signOutWithGoogle = credentials => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+
+  firebase.logout().then(() => {
+    dispatch({ type: 'SIGN_OUT_WITH_GOOGLE' });
+  });
+};
+
+export const signIn = credentials => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+
+  credentials
+    ? firebase
+        .login({
+          email: credentials.email,
+          password: credentials.password
+        })
+        .then(() => {
+          dispatch({ type: 'SIGN_IN' });
+        })
+    : firebase
+        .login({
+          provider: 'google',
+          type: 'popup'
+        })
+        .then(() => {
+          dispatch({ type: 'SIGN_IN' });
+        });
+
+  // firebase
+  //   .login({
+  //     provider: 'google',
+  //     type: 'popup'
+  //   })
+  //   .then(() => {
+  //     dispatch({ type: 'SIGN_IN' });
+  //   });
+};
+
+export const signOut = () => (dispatch, getState, { getFirebase }) => {
+  const firebase = getFirebase();
+
+  firebase.logout().then(() => {
+    dispatch({ type: 'SIGN_OUT' });
+  });
+};
+
+export const signUp = user => (dispatch, getState, { getFirebase }) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+
+  firebase
+    .createUser({
+      email: user.email,
+      password: user.password,
+      username: user.username
+    })
+    .then(res => {
+      firestore
+        .collection('users')
+        .doc(res.user.uid)
+        .set({ email: user.email });
+    })
+    .then(() => {
+      dispatch({ type: 'SIGN_UP' });
+      history.push('/create');
+    });
+};
 
 export const createCvApplication = formValues => async (
   dispatch,
