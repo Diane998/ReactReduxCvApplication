f88fa4f08487a3927e542c2dc5bd9048d3671fd3
Diane998 <gutierrez.almiradyan@gmail.com>
2020-12-04 05:49:28 +0800

    Error handling

--- src/reducers/cvApplicationReducer.js
+++ src/reducers/cvApplicationReducer.js
@@ -1,9 +1,11 @@
-const cvApplicationReducer = (state = {}, action) => {
+const INITIAL_STATE = { createCvError: null };
+
+const cvApplicationReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'CREATE_CV_APPLICATION':
-      return { ...state, [action.payload.docId]: action.payload };
+      return { ...state };
     case 'CREATE_CV_APPLICATION_ERROR':
-      return state;
+      return { ...state, createCvError: action.payload };
     default:
       return state;
   }
