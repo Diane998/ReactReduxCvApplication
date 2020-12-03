f8a2b732220b56f34864896bc6185ed34d6f9efc
Diane998 <gutierrez.almiradyan@gmail.com>
2020-12-04 05:50:56 +0800

    Error handling

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,17 +1,23 @@
-const INITIAL_STATE = {};
+const INITIAL_STATE = { authError: null };
 
 const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'SIGN_IN_WITH_GOOGLE':
       return { ...state };
+    case 'SIGN_IN_WITH_GOOGLE_ERROR':
+      return { ...state, authError: action.payload };
     case 'SIGN_OUT_WITH_GOOGLE':
       return { ...state };
     case 'SIGN_IN':
       return { ...state };
+    case 'SIGN_IN_ERROR':
+      return { ...state, authError: action.payload };
     case 'SIGN_OUT':
       return { ...state };
     case 'SIGN_UP':
       return { ...state };
+    case 'SIGN_UP_ERROR':
+      return { ...state, authError: action.payload };
     default:
       return state;
   }
